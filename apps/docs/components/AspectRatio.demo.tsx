2c338e2a1430f2ba9d2dd6df5a802b4318da9ccf
Caleb Carithers <calebcarithers@me.com>
2023-02-11 16:59:49 -0600

    up

--- apps/docs/components/AspectRatio.demo.tsx
+++ apps/docs/components/AspectRatio.demo.tsx
@@ -6,10 +6,40 @@ import SubDemo from "./SubDemo";
 const AspectRatioDemo = () => {
   return (
     <Demo title={"AspectRatio"}>
-      <div className={css("grid", "grid-cols-2")}>
-        <SubDemo labels={{ ratio: "1:1" }}>
+      <div className={css("grid", "grid-cols-4", "gap-4")}>
+        <div>
+          <SubDemo labels={{ ratio: "1/1" }}>
+            <AspectRatio
+              ratio={1}
+              className={css(
+                "bg-cover",
+                "max-w-[300px]",
+                "border-[1px]",
+                "border-black"
+              )}
+              style={{
+                backgroundImage: `url(https://d2w9rnfcy7mm78.cloudfront.net/19166408/original_38d17169efe9992829d9af7059d8d45a.png?1669558317?bc=0)`,
+              }}
+            />
+          </SubDemo>
+          <SubDemo labels={{ ratio: "3/4" }}>
+            <AspectRatio
+              ratio={"3/4"}
+              className={css(
+                "bg-cover",
+                "max-w-[300px]",
+                "border-[1px]",
+                "border-black"
+              )}
+              style={{
+                backgroundImage: `url(https://d2w9rnfcy7mm78.cloudfront.net/3135337/original_eda428c3d6672013f86876b31d6b2f18.jpg?1543775055?bc=1)`,
+              }}
+            />
+          </SubDemo>
+        </div>
+        <SubDemo labels={{ ratio: "1/3" }}>
           <AspectRatio
-            ratio={1}
+            ratio={"1/3"}
             className={css(
               "bg-cover",
               "max-w-[300px]",
@@ -17,21 +47,7 @@ const AspectRatioDemo = () => {
               "border-black"
             )}
             style={{
-              backgroundImage: `url(https://d2w9rnfcy7mm78.cloudfront.net/19166408/original_38d17169efe9992829d9af7059d8d45a.png?1669558317?bc=0)`,
-            }}
-          />
-        </SubDemo>
-        <SubDemo labels={{ ratio: "3:4" }}>
-          <AspectRatio
-            ratio={"3/4"}
-            className={css(
-              "bg-cover",
-              "max-w-[300px]",
-              "border-[1px]",
-              "border-black"
-            )}
-            style={{
-              backgroundImage: `url(https://d2w9rnfcy7mm78.cloudfront.net/3135337/original_eda428c3d6672013f86876b31d6b2f18.jpg?1543775055?bc=1)`,
+              backgroundImage: `url(https://res.cloudinary.com/dpooqlfdf/image/upload/v1676153842/tdxbcr3ryoqz8mdcdcs5.jpg)`,
             }}
           />
         </SubDemo>

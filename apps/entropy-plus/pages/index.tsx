2a90bb636b1e290c806bac155b26eb7ba26d02e0
Caleb Carithers <calebcarithers@me.com>
2023-02-13 02:16:52 -0600

    up

--- apps/entropy-plus/pages/index.tsx
+++ apps/entropy-plus/pages/index.tsx
@@ -5,12 +5,14 @@ import AuthedLayout from "../layouts/Authed.layout";
 export default function Web() {
   return (
     <AuthedLayout>
-      <div className={css("flex", "flex-wrap", "h-full")}>
-        {new Array(500).fill(undefined).map((_, index) => (
-          <Text intent={TextIntent.Outline} size={TextSize.Xl} bold>
-            + {index === 250 && "entropy"}
-          </Text>
-        ))}
+      <div className={css("flex", "items-center", "h-full")}>
+        <div className={css("flex", "flex-wrap")}>
+          {new Array(500).fill(undefined).map((_, index) => (
+            <Text intent={TextIntent.Outline} size={TextSize.Xl} bold>
+              + {index === 250 && "entropy"}
+            </Text>
+          ))}
+        </div>
       </div>
     </AuthedLayout>
   );

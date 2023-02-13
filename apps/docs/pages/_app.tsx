7011e7c7378e874ac9af18404f06f65be1ce7fbc
Caleb Carithers <calebcarithers@me.com>
2023-02-12 20:34:40 -0600

    up

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -16,10 +16,24 @@ const helvetica = localFont({
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const description = "e+ DSL";
+  const name = "e+ DSL";
+  const twitterCardUrl = "https://entropy-web-docs.vercel.app/twitter-card.png";
+  const url = "https://entropy-web-docs.vercel.app/";
   return (
     <>
       <Head>
         <title>e+ DSL</title>
+        <meta name="description" content={description} key="desc" />
+        <meta property="og:site_name" content={name} />
+        <meta property="og:title" content={name} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={twitterCardUrl} />
+        <meta property="og:url" content={url} />
+        <meta name="twitter:title" content={name} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={twitterCardUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <main className={`${helvetica.variable}`}>
         <Component {...pageProps} />

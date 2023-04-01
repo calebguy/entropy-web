6c7aab442ac87609d0db8de9dfa308a830f8355e
Brian Curran <curranbriant@gmail.com>
2023-04-01 01:23:26 -0400

    env test

--- apps/entropy-plus/environment/staging.ts
+++ apps/entropy-plus/environment/staging.ts
@@ -1,7 +1,15 @@
 import { Env } from ".";
+
+const proxyUrl = "https://entropyplus.xyz/";
+// const proxyUrl = null;
+
 const env: Env = {
-  app: { name: "[STAGING] E+" },
-  api: { baseUrl: "" },
+  app: { name: "[LOCAL] E+" },
+  api: { baseUrl: "https://entropyplus.xyz" },
 };
+
+if (proxyUrl) {
+  env.api.baseUrl = proxyUrl;
+}
 
 export default env;
